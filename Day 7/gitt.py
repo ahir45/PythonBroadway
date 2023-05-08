37893922cac89fc3065d93efd7e8a86e104a9e17
Mamba <touabhi45@gmail.com>
2023-05-08 19:26:31 +0545

    Message

--- Day 7/gitt.py
+++ Day 7/gitt.py
@@ -15,13 +15,18 @@
               # to setup a new username/email for particular project
  # git config user.email ""
 
- # git branch  # It shows all the branches and highlights the current branch
- # git branch -M main
- # main is used instead of master after black lives matter protest
 
 # SSH key to generate public key for github and private key for local device to communicate between eachother
 # ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
 # use cmd to generate ssh key using type filename
 
-#
+#### GIT BRANCHING
+
+# git <branch_name>          # creates a new branch
+# git checkout <branch_name>        # jumps (check outs ) to the next branch
+# git branch                         # It shows all the branches and highlights the current branch
+# git branch -M main
+# git checkout -b <branch_name>          # creates a new branch and jumps to that branch
+# main is used instead of master after black lives matter protest
+
 
