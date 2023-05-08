2e700ed9c5a8712fbd870e5713e5d128dbd1694e
Mamba <touabhi45@gmail.com>
2023-05-08 19:04:28 +0545

    Message

--- Day 7/gitt.py
+++ Day 7/gitt.py
@@ -1,2 +1,27 @@
+# git config --global user.name ""
+# git config --global user.email ""
+
+
 # git init
-# git add.
+# git add .  # git add filename
+# git commit -m "<commit message>"
+# git remote add origin <giturl>
+# git push -u origin <branch_name>
+
+
+# clear
+# git status     #  shows the current status of all git staged and unstaged files
+
+              # to setup a new username/email for particular project
+ # git config user.email ""
+
+ # git branch  # It shows all the branches and highlights the current branch
+ # git branch -M main
+ # main is used instead of master after black lives matter protest
+
+# SSH key to generate public key for github and private key for local device to communicate between eachother
+# ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+# use cmd to generate ssh key using type filename
+
+#
+
